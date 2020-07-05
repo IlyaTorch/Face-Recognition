750d345ca6ede5f42ae8cc0450ffc8eb3edfd205
IlyaTorch <itorch2001@gmail.com>
2020-07-05 22:33:26 +0300

    add saving urls to db from form

--- web/app/face/views.py
+++ web/app/face/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Url
 
 
 def index(request):
     if request.method == 'POST':
-        print(request.POST.get('image_url'))
-    context = {'data': 'face app'}
+
+        url = Url.objects.create(image_url=request.POST.get('image_url'))
+        url.save()
+
+    image_urls = Url.objects.all()
+
+    context = {'data': image_urls}
     return render(request, 'face/index.html', context=context)
