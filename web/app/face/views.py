2fd46b8653953b5347dfccaea352abc7e45e4ce4
IlyaTorch <itorch2001@gmail.com>
2020-07-07 14:10:58 +0300

    add functionality of face detection

--- web/app/face/views.py
+++ web/app/face/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Url
+from django.shortcuts import render, redirect
+from .models import Url, BoundingBox
+import cv2
+from imutils import url_to_image 
+
+face_cascade = cv2.CascadeClassifier('/haarcascade_frontalface_default.xml')
 
 
 def index(request):
@@ -7,6 +11,27 @@ def index(request):
 
         url = Url.objects.create(image_url=request.POST.get('image_url'))
         url.save()
+
+        img = url_to_image(request.POST.get('image_url'))
+        ih, iw, _ = img.shape
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            top = round(y * 100 / ih, 2)
+            right = round((iw - x - w) * 100 / iw, 2)
+            left = round(x * 100 / iw, 2)
+            bottom = round((ih - y - h) * 100 / ih, 2)
+            bounding_box = BoundingBox.objects.create(top=top,
+                                                      right=right,
+                                                      left=left,
+                                                      bottom=bottom,
+                                                      image=url)
+            bounding_box.save()
+            
+
+        return redirect('/face')
 
     image_urls = Url.objects.all()
 
